f5e5108557baeea0cec018bdfa14a1de8ae36726
Vadim Steshenko <vadim@minddnim.com>
2020-04-12 21:28:17 +0300

    Update to swift tool version 5.2

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
@@ -10,11 +10,19 @@ let package = Package(
         .library(name: "LeafKit", targets: ["LeafKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.0.0-beta.2.1")),
+        .package(url: "https://github.com/vapor/vapor.git", .upToNextMajor(from: "4.0.1")),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.2.0"),
     ],
     targets: [
-        .target(name: "LeafKit", dependencies: ["NIO", "Vapor"]),
-        .testTarget(name: "LeafKitTests", dependencies: ["LeafKit", "XCTVapor"]),
+        .target(name: "LeafKit", dependencies: [
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "Vapor", package: "vapor"),
+            ]
+        ),
+        .testTarget(name: "LeafKitTests", dependencies: [
+            .target(name: "LeafKit"),
+            .product(name: "XCTVapor", package: "vapor"),
+            ]
+        ),
     ]
 )
